2659b4ff4dd28137234869cf3f20cf39c720c01e
zhouxihua <zxh820303380@163.com>
2014-02-24 16:26:51 +0800

    改变了

---
+++ maven-ssh/src/test/java/com/dhcc/ssh/test/controller/LoginControllerTest.java
@@ -0,0 +1,14 @@
+package com.dhcc.ssh.test.controller;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class LoginControllerTest {
+
+	@Test
+	public void testHandleRequest() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ maven-ssh/src/test/java/com/dhcc/ssh/test/service/UserServiceImplTest.java
@@ -0,0 +1,14 @@
+package com.dhcc.ssh.test.service;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class UserServiceImplTest {
+
+	@Test
+	public void testVerifyUserByUserNameAndPassword() {
+		fail("Not yet implemented");
+	}
+
+}
